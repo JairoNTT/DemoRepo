bc34792d3b4d5b22ccf4ee3b8302e14336217709
JairoNTT <jelyazid@emeal.nttdata.com>
2025-11-03 18:21:45 +0100

    Remove project files during repo reset

--- person-rest/src/main/java/com/jairo/repository/InsuredObjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jairo.repository;
-
-import com.jairo.model.InsuredObject;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InsuredObjectRepository extends CrudRepository<InsuredObject, Integer> { }
